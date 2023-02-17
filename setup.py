fa6cb9f170b71bcb5bf8cee7384ced8c47ec34d7
sami92news <sami.akram@92newshd.tv>
2023-02-17 17:03:27 +0500

    first

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python
-import sys
-sys.stderr.write("""This version does not support Python 2.7 any longer""")
-sys.exit(1)
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='number_utility',
+    version='0.0.3',
+    description='Test package.',
+    py_modules=["samtestpip"],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Sami Akram S M",
+    author_email="samiakram@live.com",
+    url="https://github.com/humblesami/samtestpip"
+)
